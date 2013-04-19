66be46df4d30a1f27572adab56a4365f176b35a6
Tom Celinski <tomcelinski@hotmail.com>
2013-04-20 00:12:10 +1000

    Update page-faq.php
    Added a bunch of FAQ items

--- page-faq.php
+++ page-faq.php
@@ -11,17 +11,19 @@ get_header(); ?>
 	 	<ol>
 	 		<li><a href="#interest">Have you had a lot of interest in the Mini Aero?<span>&raquo;</span></a></li>
 	 		<li><a href="#production">When will the Mini Aero be in production? <span>&raquo;</span></a></li>
-	 		<li><a href="#(insert ID here)">(Insert question here) <span>&raquo;</span></a></li>
-	 		<li><a href="#support">How will you provide customer support? <span>&raquo;</span></a></li>
- 			<li><a href="#volume">Is the volume control done analog or digital domain? <span>&raquo;</span></a></li>
- 			<li><a href="#edges">Why do the Mini Aero not have rounded edges? <span>&raquo;</span></a></li>
- 			<li><a href="#work">How does the Mini Aero work? <span>&raquo;</span></a></li>
- 			<li><a href="#creditcard">Where will my credit card be charged? <span>&raquo;</span></a></li>
- 			<li><a href="#spec">What is the spec for the Mini Aero? <span>&raquo;</span></a></li>
- 			<li><a href="#grilles">Will the Mini Aero come with grilles? <span>&raquo;</span></a></li>
- 			<li><a href="#sources">How can I connect my sources to the Mini Aero? <span>&raquo;</span></a></li>
- 			<li><a href="#playback">How do I control playback on the Mini Aero? <span>&raquo;</span></a></li>
-	 	</ol>
+	 		<li><a href="#countryavailable">Will the Mini Aero be available in my country? <span>&raquo;</span></a></li>
+ 			<li><a href="#spec">Is there a spec for the Mini Aero? <span>&raquo;</span></a></li>
+	 		<li><a href="#playbackcontrol">How will I control playback? <span>&raquo;</span></a></li>
+ 			<li><a href="#volumecontrol">How will I control volume? <span>&raquo;</span></a></li>
+ 			<li><a href="#volumeanalogdigital">Is the volume control done analog or digital domain? <span>&raquo;</span></a></li>
+ 			<li><a href="#legacysources">Can I connect my CD, DVD or PluRay player to the Mini Aero? <span>&raquo;</span></a></li>
+ 			<li><a href="#wiredconnectivity">Will the Mini Aero have wired connectivity? <span>&raquo;</span></a></li>
+ 			<li><a href="#grilles"> Will the Mini Aero come with grilles? <span>&raquo;</span></a></li>
+ 			<li><a href="#remotecontrol">Will the Mini Aero come with an IR remote control? <span>&raquo;</span></a></li>
+ 			<li><a href="#stands">Will you have matching stands for the Mini Aero? <span>&raquo;</span></a></li>
+ 			<li><a href="#inputhub">Will there be a way of connecting CD, DVD or BluRay Player in the future? <span>&raquo;</span></a></li>
+			<li><a href="#subwoofer">Will there be a matching subwoofer? <span>&raquo;</span></a></li>
+		</ol>
 	 </div>
 </section>
 
@@ -70,14 +72,90 @@ get_header(); ?>
 	 </div>
 </section>
 
-<section class="light clear answer" id="( insert ID here)">
+
+<section class="light clear answer" id="countryavailable">
 	 <div class="wrapper">
-	 	<h2><span>Q3.</span> ( Insert question name here )</h2>
-	 	<div class="inset-text"><p>First, to everyone who pre-ordered the product - your commitment and support matters immensely to us.  We appreciate your patience and assure you we will deliver great wireless speakers that are unlike any other wireless audio product in existence today when it comes to audio quality.</p>
+	 	<h2><span>Q3.</span> Will the Mini Aero be available in my country?</h2>
+	 	<div class="inset-text"><p>The short answer is YES! The long answer is that we are working through the complexities of import rules and taxes, and how to collect these given we are selling direct. In the short term we will definitely arrange delivery outside of US and Australia on a case-by case basis. Please don’t hesitate to contact us if you have any questions regarding this – we would be glad to answer any questions.</p>
 
-<p>At this stage we expect the Mini Aero to be in production in about 10 weeks.  Yes, we are running late – guilty as charged. Wireless speakers are complex, and frankly we underestimated the amount of time it takes to get some of the design aspects just right. And yes, we want to get everything 100% right before shipping, or as close as possible. Too many companies ship half-baked products to meet deadlines. We are not one of them – never will be. While deadlines are important to us, and we will absolutely do our best to meet them, quality comes first every time – we will never compromise on this.</p>
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
 
-</div>
+<section class="light clear answer" id="spec">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> Is there a spec for the Mini Aero?</h2>
+	 	<div class="inset-text"><p>We have preliminary specs, but our preference at this point is not to put out a preliminary spec on the small chance final measurements will differ somewhat. The aim here is to avoid different versions of spec floating on the internet, with potential for causing confusion.</p>
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="playbackcontrol">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> WillHow will I control playback?</h2>
+	 	<div class="inset-text"><p>The Mini Aero come with a USB dongle for your PC/Mac.  This allows playback from a music library on a PC/Mac - in fact any sound output from your PC/Mac will be redirected to the Mini Aero.</p>
+		<p>From the perspective of remotely controlling music playback on your PC/Mac, the best option is to use a smart phone app in conjunction with a matching music library.  There are many great remote control apps for iPhone/iPod/iPad, for Android phones and tablets, and for Windows phones.  Many of these are free and those that are not free cost a nominal amount. We will put together a list of recommended remote control apps and make this list available here at a future point in time. </p>
+		<p>The great thing about apps is that they give you full access to your music library, as well as allowing control of playback and volume. Those who don't own a smart phone, or prefer to have a dedicated non-phone device as remote control have two options:
+			<ol>
+			<li>Using an Android tablet – they start at around $100 for low-end models; $199 buys the excellent Google Nexus 7. There are other nice tablets at that price.</li>
+			<li>Using an iPod touch on an iPad – there is a great selection of apps for virtually every media library as well as many streaming services, such as Spotify and others.</li>
+			</ol>
+		</p>
+		<p>The full functionality provided by an app could never be matched by an IR remote, as there is no way of allowing browsing of your music library from an IR remote. It is for this reason that we don't provide a standard remote with the Mini Aero.</p>
+		<p>BUT... we are also developing an <a href="#inputhub">input hub</a> product. The input hub will take a range of inputs from standard audio sources and will allow the Mini Aero to be used in a much more traditional way, with no PC/Mac involved. Because of this it makes sense to provide an IR remote.</p>
+		
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="volumecontrol">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> How will I control volume?</h2>
+	 	<div class="inset-text"><p>Volume is controlled using an app – please see <a href="#playbackcontrol">How will I control playback?</a> for details. </p>
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="volumeanalogdigitala">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> Is the volume control done analog or digital domain?</h2>
+	 	<div class="inset-text"><p>The volume control is done in the digital domain.</p>
+
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="legacysources">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> Can I connect my CD, DVD or Blu-Ray player to the Mini Aero?</h2>
+	 	<div class="inset-text"><p>No, you cannot connect your CD, DVD or Blu-Ray directly to the Mini Aero. You will however be able to connect various sources via our <a href="#inputhub">input hub</a> product, which will be available later this year.</p>
+
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="wiredconnectivity">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> Will the Mini Aero have wired connectivity in addition to wireless?</h2>
+	 	<div class="inset-text"><p>No, the Mini Aero will not have wired connectivity in addition to wireless – here by wired connectivity we mean running audio over a wire to the speakers, and running a wire from one speaker to the other. We have thought hard about this. To properly support wired connectivity requires the inclusion of a considerable amount of additional electronics in what is already very limited space. To do this well we would not only sacrifice space inside the speaker, but would also add non-trivial amount of cost. We don’t believe this would add any significant benefits to the functionality or the operation of the Mini Aero, particularly in the context of the input hub which will accommodate a range of sources/inputs. </p>
+
+		</div>
+	 	<a class="top" href="#top">back to top</a>
+	 </div>
+</section>
+
+<section class="light clear answer" id="countryavailable">
+	 <div class="wrapper">
+	 	<h2><span>Q3.</span> How will I control volume?</h2>
+	 	<div class="inset-text"><p></p>
+
+		</div>
 	 	<a class="top" href="#top">back to top</a>
 	 </div>
 </section>
